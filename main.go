360dfcb70aefab089077629045372e5920b05c71
kkloberdanz <kyle.g.kloberdanz@gmail.com>
2021-03-14 19:10:39 -0500

    storing file to staging directory and hashing file

--- main.go
+++ main.go
@@ -19,8 +19,88 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	uuid "github.com/satori/go.uuid"
 )
 
+const (
+	KFS_VERSION      = "0.0.1"
+	KFS_STORAGE_PATH = "/home/kyle/.kfs/storage"
+	KFS_STAGING_PATH = "/home/kyle/.kfs/staging"
+)
+
+func index(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "KFS version: %s", KFS_VERSION)
+}
+
+func get_output_path(input_filename string) string {
+	extension := filepath.Ext(input_filename)
+	output_id := uuid.Must(uuid.NewV4(), nil)
+	output_name := fmt.Sprintf("%s%s", output_id, extension)
+	output_path := filepath.Join(KFS_STAGING_PATH, output_name)
+	return output_path
+}
+
+func store_file(filename string) {
+	fmt.Printf("storing: %s\n", filename)
+	output, err := exec.Command("sha256sum", filename).Output()
+	if err != nil {
+		fmt.Printf("failed to hash '%s': %s\n", filename, err)
+		return
+	}
+
+	output_str := string(output)
+	hash := strings.Fields(output_str)[0]
+	fmt.Printf("hash = %s\n", hash)
+}
+
+/**
+ * Receive file, and write it to the staging directory.
+ * When finished receiving file, run background routine to persist it to
+ * durable storage and add it to the disk array.
+ */
+func handle_upload(writer http.ResponseWriter, request *http.Request) {
+	// you can upload file with:
+	// curl -X POST -F "file=@src/main.go" localhost:8080/upload
+	file, header, err := request.FormFile("file")
+	if err != nil {
+		http.Error(
+			writer,
+			"file upload requires key of 'file'",
+			http.StatusBadRequest,
+		)
+		fmt.Fprintf(writer, "error\n")
+		return
+	}
+	defer file.Close()
+	fmt.Printf("got file '%s'\n", header.Filename)
+	output_path := get_output_path(header.Filename)
+	outf, err := os.Create(output_path)
+	if err != nil {
+		fmt.Printf("failed to create output file: %s\n", err)
+		return
+	}
+	defer outf.Close()
+	io.Copy(outf, file)
+	go store_file(output_path)
+}
+
 func main() {
-	fmt.Println("Hello world!")
+	fmt.Println("KFS -- Kyle's File Storage")
+	fmt.Printf("version: %s\n", KFS_VERSION)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", index)
+	mux.HandleFunc("/upload", handle_upload)
+	server := &http.Server{
+		Addr:    "0.0.0.0:8080",
+		Handler: mux,
+	}
+	log.Fatal(server.ListenAndServe())
 }
